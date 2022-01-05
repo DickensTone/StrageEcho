fe8cce4e0d1aa5ef1ff28ec1ca47e21b3f36f1a4
ddt <daiij@qq.com>
2022-01-05 23:33:50 +0800

    use NacosValue to inject value

--- server/src/main/java/com/echo/server/configuration/NacosConfig.java
+++ server/src/main/java/com/echo/server/configuration/NacosConfig.java
@@ -2,10 +2,13 @@ package com.echo.server.configuration;
 
 import com.alibaba.nacos.api.annotation.NacosProperties;
 import com.alibaba.nacos.spring.context.annotation.EnableNacos;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableNacos(globalProperties = @NacosProperties(serverAddr = "${nacos.server-addr}"))
+@NacosPropertySource(dataId = "server_application", autoRefreshed = true)
+@NacosPropertySource(dataId = "dataId", autoRefreshed = true)
 public class NacosConfig {
     
 }
--- server/src/test/java/com/echo/server/TestNacos.java
+++ server/src/test/java/com/echo/server/TestNacos.java
@@ -3,13 +3,17 @@ package com.echo.server;
 
 import com.alibaba.nacos.api.annotation.NacosInjected;
 import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.echo.server.configuration.NacosConfig;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.UUID;
 
 import static com.alibaba.nacos.api.common.Constants.DEFAULT_GROUP;
 import static com.alibaba.nacos.client.config.common.ConfigConstants.DATA_ID;
@@ -22,11 +26,25 @@ public class TestNacos {
 
     @NacosInjected
     private ConfigService configService;
+
+    @NacosValue(value = "${uuid:helloBug}", autoRefreshed = true)
+    private String uuid;
     
     
     @Test
-    public void test() throws NacosException {
-        configService.publishConfig(DATA_ID, DEFAULT_GROUP, "9527");
+    public void test() throws NacosException, InterruptedException {
+        Assert.assertNotEquals(uuid, "helloBug");
+        String content = UUID.randomUUID().toString();
+        configService.publishConfig(DATA_ID, DEFAULT_GROUP, "uuid="+content);
+        
+        //give nacos some time to save the published content.
+        Thread.sleep(1000);
+        String nowContent = configService.getConfig(DATA_ID, DEFAULT_GROUP, 200);
+        nowContent = nowContent.split("=")[1];
+
+        Assert.assertEquals(content, nowContent);
+        Assert.assertEquals(uuid, content);
+        
     }
     
 }
